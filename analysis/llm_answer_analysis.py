315ba6fe89d4e4f66418d4c1365ea06fca960a62
Leuson Mario <leusonmario@gmail.com>
2023-05-22 23:18:36 -0400

    comparing generated and original answer

---
+++ analysis/llm_answer_analysis.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer, util
+
+
+class GeneratedAnswerAnalysis:
+
+    def __init__(self, model):
+        self.model = SentenceTransformer(model)
+
+
+    def check_cosine_metric(self, original_answer, generated_answer):
+        embedding_original_answer = self.model.encode(original_answer, convert_to_tensor=True)
+        embedding_general_answer = self.model.encode(generated_answer['choices'][0]['text'], convert_to_tensor=True)
+
+        cosine_metric_value = util.cos_sim(embedding_original_answer, embedding_general_answer)
+
+        return cosine_metric_value[0][0]
---
+++ llm/request.py
@@ -0,0 +1,21 @@
+import openai
+from decouple import config
+
+
+class Request:
+
+    def __init__(self):
+        openai.api_key = config("openai-key")
+
+    def generate_prompt(self, question, body):
+        return f""" Please, explain how to fix the problem below. {question}
+        Below, you can find more details 
+        {body}"""
+
+    def ask_for_explanation(self, question, description):
+        return openai.Completion.create(
+            model="text-davinci-003",
+            prompt=self.generate_prompt(question, description),
+            temperature=0.6,
+            max_tokens=2048
+        )
